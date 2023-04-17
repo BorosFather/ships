154398dbdbf22bcbb137fb5ab60d9bc536033317
BorosFather <boros.zoli024@gmail.com>
2023-04-17 17:47:10 +0200

    ship, about & shered könyvtárak létresozva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShipComponent } from './ship/ship.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShipComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
